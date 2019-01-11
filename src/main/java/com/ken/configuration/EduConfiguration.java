bbb0102410b7b7c56d8f550ee7efaefb948081f1
yao_hqi <yao_hqi@example.com>
2019-01-11 18:27:44 +0800

    .

--- src/main/java/com/ken/configuration/EduConfiguration.java
+++ src/main/java/com/ken/configuration/EduConfiguration.java
@@ -1,5 +1,9 @@
 package com.ken.configuration;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,5 +16,35 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @ComponentScan("com.ken")
 public class EduConfiguration {
+
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer scannerConfigurer = new MapperScannerConfigurer();
+        scannerConfigurer.setBasePackage("com.ken.mapper");
+        return scannerConfigurer;
+    }
+
+    @Bean
+    public MybatisSqlSessionFactoryBean sqlSessionFactoryBean() {
+        MybatisSqlSessionFactoryBean sqlSessionFactoryBean = new MybatisSqlSessionFactoryBean();
+        return sqlSessionFactoryBean;
+    }
+
+    @Bean
+    public DruidDataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl("");
+        dataSource.setUsername("");
+        dataSource.setPassword("");
+        dataSource.setInitialSize(1);
+        dataSource.setMinIdle(1);
+        dataSource.setMaxActive(20);
+        dataSource.setMaxWait(60000);
+        dataSource.setTimeBetweenEvictionRunsMillis(60000);
+        dataSource.setMinEvictableIdleTimeMillis(300000);
+        dataSource.setPoolPreparedStatements(true);
+        dataSource.setMaxPoolPreparedStatementPerConnectionSize(20);
+        return dataSource;
+    }
 
 }
---
+++ src/main/java/com/ken/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.ken.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ken.entity.User;
+
+/**
+ * @author yhq
+ * @date 2019/1/11
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
